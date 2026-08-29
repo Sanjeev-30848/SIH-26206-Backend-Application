3d5d614dc4e2638916f3b7c62990dd5b332fc087
Sanjeev <jagansanjeev2007@gmail.com>
2026-08-29 19:08:09 +0530

    Added DTO's

--- src/main/java/com/klef/sih/dto/UserResponse.java
+++ src/main/java/com/klef/sih/dto/UserResponse.java
@@ -1,6 +1,28 @@
 package com.klef.sih.dto;
 
+import java.time.LocalDateTime;
+
+import com.klef.sih.entity.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse 
 {
 
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private Role role;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
