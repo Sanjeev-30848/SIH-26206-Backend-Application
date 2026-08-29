ccfd7f0f8684f4fec93abd15987473f47f6b78e7
Sanjeev <jagansanjeev2007@gmail.com>
2026-08-29 19:19:26 +0530

    Added User and Swagger API

--- src/main/java/com/klef/sih/config/SwaggerConfig.java
+++ src/main/java/com/klef/sih/config/SwaggerConfig.java
@@ -1,6 +1,24 @@
 package com.klef.sih.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig 
 {
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PRANA Disaster Management System API")
+                        .version("1.0")
+                        .description(
+                                "REST API for PRANA Disaster Preparedness and Response System"
+                        ));
+    }
 }
--- src/main/java/com/klef/sih/controller/UserController.java
+++ src/main/java/com/klef/sih/controller/UserController.java
@@ -1,16 +1,80 @@
 package com.klef.sih.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.klef.sih.entity.User;
+import com.klef.sih.service.UserService;
 
 @RestController
-@RequestMapping("/user")
-public class UserController 
-{
-	@GetMapping("/")
-	public String home()
-	{
-		return "PRANA - SIH Disaster Management Project";
-	}
+@RequestMapping("/api/users")
+@CrossOrigin
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) 
+    {
+        this.userService = userService;
+    }
+
+    // Add User
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+
+        User savedUser = userService.addUser(user);
+
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    // Get User by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+
+        User user = userService.getUserById(id);
+
+        return ResponseEntity.ok(user);
+    }
+
+    // Get All Users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        List<User> users = userService.getAllUsers();
+
+        return ResponseEntity.ok(users);
+    }
+
+    // Update User
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable Long id,
+            @RequestBody User user) {
+
+        User updatedUser = userService.updateUser(id, user);
+
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    // Delete User
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+
+        userService.deleteUser(id);
+
+        return ResponseEntity.ok("User deleted successfully");
+    }
+
+    // Get User by Email
+    @GetMapping("/email/{email}")
+    public ResponseEntity<User> getUserByEmail(
+            @PathVariable String email) {
+
+        User user = userService.getUserByEmail(email);
+
+        return ResponseEntity.ok(user);
+    }
 }
