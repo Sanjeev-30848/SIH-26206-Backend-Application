b7522fdcdaaa77c67e16b93ce8b6feb2b1b1285d
Sanjeev <jagansanjeev2007@gmail.com>
2026-08-29 12:11:11 +0530

    Created packages

---
+++ src/main/java/com/klef/sih/SihSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.klef.sih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SihSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SihSpringBootApplication.class, args);
+		System.out.println("Backend Application is running...!!!");
+	}
+
+}
---
+++ src/test/java/com/klef/sih/SihSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klef.sih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SihSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
